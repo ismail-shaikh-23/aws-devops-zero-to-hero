f40f54abce4ea232314056943bd6dcdd0a14d751
ismail shaikh <ismail23sk23@gmail.com>
2026-03-19 13:19:57 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,6 @@ set -e
 
 # Pull the Docker image from Docker Hub
 docker pull ismailshaikh23/sample-app-codepipeline:latest
-docker ps
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 ismailshaikh23/sample-app-codepipeline:latest
